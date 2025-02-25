c25b30f5ab1ba39f1752205d28ec27721c6c6405
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2025-02-25 13:11:17 +0700

    add registrasion logic

---
+++ functions.php
@@ -0,0 +1,51 @@
+<?php
+
+// buat koneksi
+$conn = mysqli_connect("localhost", "root", "", "mytodo");
+
+function registrasi($data)
+{
+    global $conn;
+    $username = strtolower(stripslashes($data["username"]));
+    $password = mysqli_escape_string($conn, $data["password"]);
+    $password2 = mysqli_escape_string($conn, $data["password2"]);
+
+    // cek username sudah ada atau belum
+    $result = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$username'");
+
+    // pengecekan username
+    if (strlen($username) < 8) {
+        echo "<script>
+        alert('username kurang panjang!')
+        </script>";
+        return 0;
+    } else if (mysqli_fetch_assoc($result)) {
+        echo "<script>
+        alert('username sudah digunakan!')
+        </script>";
+        return 0;
+    }
+
+
+    // pengecekan password
+    if (strlen($password) < 8) {
+        echo "<script>
+        alert('password kurang panjang!')
+        </script>";
+        return 0;
+    }
+
+    if ($password !== $password2) {
+        "<script>
+        alert('password tidak sama!')
+        </script>";
+        return 0;
+    }
+
+    // enkripsi password
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    mysqli_query($conn, "INSERT INTO tb_user VALUES ('', '$username', '$password')");
+
+    return mysqli_affected_rows($conn);
+}
--- register.php
+++ register.php
@@ -1,5 +1,18 @@
 <?php
 require "functions.php";
+
+if (isset($_POST["submit"])) {
+    if (registrasi($_POST) != 0) {
+        echo "<script>
+        alert('data berhasil ditambahkan!')
+        </script>";
+    } else {
+        // $error = "terjadi kesalahan dalam menambahkan data";
+        mysqli_error($conn);
+    }
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -9,14 +22,27 @@ require "functions.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MyTodo - Register</title>
+    <style>
+        .warning {
+            color: red;
+            font-weight: bold;
+            font-size: 20px;
+            margin-bottom: 10px;
+        }
+    </style>
 </head>
 
 <body>
     <h1>MyTodo</h1>
+    <?php if (isset($error)): ?>
+        <div class="warning">
+            <?= $error; ?>
+        </div>
+    <?php endif ?>
     <form action="" method="post">
         <div>
             <label for="email">Username</label>
-            <input type="text" name="email" id="">
+            <input type="text" name="username" id="username">
         </div>
         <div>
             <label for="password">Password</label>
