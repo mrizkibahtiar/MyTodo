87988a8f3278b4bd5a8a75ec8f0a12e9c36a9af2
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2025-03-03 23:33:35 +0700

    Refactor registration page layout and styling for improved usability

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 require "functions.php";
 
-if (isset($_POST["submit"])) {
+if (isset($_POST["register"])) {
     if (registrasi($_POST) != 0) {
         echo "<script>
         alert('data berhasil ditambahkan!')
@@ -27,42 +27,147 @@ if (isset($_POST["submit"])) {
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
         rel="stylesheet">
     <style>
-        body {
+        * {
             font-family: "Plus Jakarta Sans", serif;
+        }
+
+        body {
+            width: 100%;
+            overflow: hidden;
         }
 
         .warning {
             color: red;
             font-weight: bold;
-            font-size: 20px;
+        }
+
+        .highlight {
+            font-size: 50px;
+        }
+
+        .container {
+            margin-inline: auto;
+            text-align: center;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        .container-form {
+            background-color: white;
+            box-shadow: 1px 5px 10px rgba(0, 0, 0, 0.1);
+            padding-inline: 25px;
+            padding-block: 30px;
+            border-radius: 10px;
+            border: 1px solid rgba(203, 203, 203, 0.87);
+        }
+
+        .container-form .username,
+        .password {
+            display: flex;
+            width: 400px;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        .container-form .username label,
+        .password label {
+            align-self: baseline;
             margin-bottom: 10px;
+            font-weight: bold;
+        }
+
+        .container-form .username input,
+        .password input {
+            box-sizing: border-box;
+            width: 100%;
+            margin-bottom: 10px;
+            height: 40px;
+            border-radius: 10px;
+            padding-inline: 10px;
+            border: 1px solid gainsboro;
+            font-size: 15px;
+        }
+
+        .container-form .username input:focus,
+        .container-form .password input:focus {
+            border: 1px solid black;
+            outline: none;
+        }
+
+        .container-form #register {
+            margin-top: 10px;
+            width: 100%;
+            height: 40px;
+            font-size: 15px;
+            font-weight: bold;
+            color: white;
+            background-color: black;
+            border-radius: 10px;
+            cursor: pointer;
+        }
+
+        .container-form #register:hover {
+            transition-duration: 440ms;
+            background-color: white;
+            color: black;
+            border: 2px solid black;
+        }
+
+        .container-login {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 10px;
+            text-align: center;
+        }
+
+        .container-login a {
+            text-decoration: none;
+            color: rgb(31, 0, 184);
+            font-weight: bolder;
+        }
+
+        .container-login a:hover {
+            color: gray;
+            transition-duration: 350ms;
         }
     </style>
 </head>
 
 <body>
-    <h1>MyTodo</h1>
-    <?php if (isset($error)): ?>
-        <div class="warning">
-            <?= $error; ?>
+    <div class="container">
+        <h1 class="highlight">MyTodo</h1>
+
+        <!-- error -->
+        <?php if (isset($error)): ?>
+            <div class="warning">
+                <?= $error; ?>
+            </div>
+        <?php endif; ?>
+
+        <div class="container-form">
+            <form action="" method="post">
+                <div class="username">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username" placeholder="username">
+                </div>
+                <div class="password">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="password">
+                </div>
+                <div class="password">
+                    <label for="password2">Konfirmasi Password</label>
+                    <input type="password" name="password2" id="password2" placeholder="konfirmasi password">
+                </div>
+                <button type="submit" name="register" id="register">Daftar</button>
+            </form>
         </div>
-    <?php endif ?>
-    <form action="" method="post">
-        <div>
-            <label for="email">Username</label>
-            <input type="text" name="username" id="username">
+        <div class="container-login">
+            <p>Sudah punya akun?</p>
+            <a href="index.php">klik disini!</a>
         </div>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password">
-        </div>
-        <div>
-            <label for="password2">Konfirmasi Password</label>
-            <input type="password" name="password2" id="password2">
-        </div>
-        <button type="submit" name="submit">Daftar</button>
-        <p>Sudah punya akun? <a href="index.php">klik disini!</a></p>
-    </form>
+    </div>
 </body>
 
 </html>
